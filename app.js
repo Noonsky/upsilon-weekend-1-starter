dc2bb2c4eecf502bc6ed0ebcc9c456bb623a8360
Erik Noonan <eriknoonan@Eriks-MacBook-Pro.local>
2017-01-08 18:12:33 -0600

    Working on expenditure calculation portion

--- app.js
+++ app.js
@@ -1,57 +1,58 @@
-var corpExpenses=0;
+var corpExpenses = 0;
 
 
-$(function () {
-  console.log('document is ready');
+$(function() {
+    console.log('document is ready');
 
-  $('form').on('submit', function (event) {
-    console.log('form submitted');
+    $('form').on('submit', function(event) {
+        console.log('form submitted');
 
-    event.preventDefault();
+        event.preventDefault();
 
-    var formData = {};
-    var formAsArray = $(this).serializeArray();
+        var formData = {};
+        var formAsArray = $(this).serializeArray();
 
-    formAsArray.forEach(function (input) {
-      formData[input.name] = input.value;
+        formAsArray.forEach(function(input) {
+            formData[input.name] = input.value;
+        });
+
+        appendDom(formData);
+
+        clearForm();
+
+        salary(formData.employeeSalary);
     });
-
-    appendDom(formData);
-
-    clearForm();
-
-    salary(formData.employeeSalary);
-  });
 
 
 });
 
 function appendDom(emp) {
-  var $emp = $('<div class="employee"></div>'); // create a div jQuery object
-  var $ul= $('<ul></ul>');
-  var $firstName=$('<li>' + emp.employeeFirstName + '</li>' );
-  var $lastName=$('<li>' + emp.employeeLastName + '</li>' );
-  var $id=$('<li>' + emp.employeeIdNumber + '</li>' );
-  var $job=$('<li>' + emp.employeeJobTitle + '</li>' );
-  var $salary=$('<li id="salary">' + emp.employeeSalary + '</li>' );
-  $ul.append($firstName);
-  $ul.append($lastName);
-  $ul.append($id);
-  $ul.append($job);
-  $ul.append($salary);
-  $emp.append($ul);
+    var $emp = $('<div class="employee"></div>'); // create a div jQuery object
+    var $ul = $('<ul></ul>');
+    var $firstName = $('<li>' + emp.employeeFirstName + '</li>');
+    var $lastName = $('<li>' + emp.employeeLastName + '</li>');
+    var $id = $('<li>' + emp.employeeIdNumber + '</li>');
+    var $job = $('<li>' + emp.employeeJobTitle + '</li>');
+    var $salary = $('<li id="salary">' + emp.employeeSalary + '</li>');
+    $ul.append($firstName);
+    $ul.append($lastName);
+    $ul.append($id);
+    $ul.append($job);
+    $ul.append($salary);
+    $emp.append($ul);
 
-  $('#employees').append($emp); // append our div to the DOM
+    $('#employees').append($emp); // append our div to the DOM
 }
 
 function clearForm() {
-  $('form').find('input[type=text]').val('');
-function clearForm() {
+    $('form').find('input[type=text]').val('');
     $('form').find('input[type=number]').val('');
 }
 
-function salary(add){
-  corpExpenses((Number(add)/12).toFixed(2));
-  $('#corpExpenses').number(corpExpenses);
+function salary(add) {
+    corpExpenses=(corpExpenses+add) / 12;
+    // $("corpExpenses").append(corpExpenses);
+    $("#corpExpenses").html(String(corpExpenses));
+
 
 }
